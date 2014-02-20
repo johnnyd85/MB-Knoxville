dabe1046c4193f25a06c1dc1fb05dc318da1cde4
Jwpe <jwpevans@gmail.com>
2014-02-19 22:28:39 -0500

    Readded jquery data table. Figured out how to pass data from an angular route to a view.

--- public/script.js
+++ public/script.js
@@ -1,103 +1,83 @@
 App = Ember.Application.create({});
 
 App.Router.map(function() {
-  this.resource('rides');
+    this.resource('rides');
 });
 
 
 App.IndexRoute = Ember.Route.extend({
-  model: function(){
-      return [
-          {firstName: 'John', lastName: 'Dillman'},
-          {firstName: 'Nick', lastName: 'Shoemaker'}
-      ];
-  }
+    model: function(){
+        return [
+            {firstName: 'John', lastName: 'Dillman'},
+            {firstName: 'Nick', lastName: 'Shoemaker'}
+        ];
+    }
 });
 
 App.RidesRoute = Ember.Route.extend({
-  model: function() {
-    var rides = this.store.find('ride');
-    console.log(rides);
-    return rides;
-  }
-});
-
-App.DataTableView =  Ember.View.extend({
-  
-  classNames:['table','table-striped','table-bordered','dataTable'],
-  tagName:'table',
-  didInsertElement: function(){
-  var self = this;
-  console.log(self);
-  
-  this.$().dataTable({
- 
-          "bProcessing": true,
-          "aaData":App.Ride,
-
-            "aoColumns": [
-            { "sTitle" : "Name", "mData": "title" },
-            { "sTitle" : "Type", "mData": "type" },
-            { "sTitle" : "Length", "mData": "length" }
-            ]
-
-        });
-	//});
-
-  }
+    model: function() {
+        return this.store.find('ride');
+    }
 });
 
 App.Ride = DS.Model.extend({
-  title: DS.attr('string'),
-  type: DS.attr('string'),
-  length: DS.attr('number')
+    title: DS.attr('string'),
+    type: DS.attr('string'),
+    length: DS.attr('number'),
 });
 
-/*App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.DataTableView =  Ember.View.extend({
 
-App.ApplicationAdapter = DS.RESTAdapter.extend({ 
-   url: 'http://localhost:3000',
-   namespace: 'rides',
-   serializer: DS.RESTSerializer.extend({
-        primaryKey: function(type) {
-            return '_id';
-        }
-   })
+    classNames:['table','table-striped','table-bordered','dataTable'],
+    tagName:'table',
+    didInsertElement: function(){
+        var self = this;
+        data = self.rides.getEach('data');
+
+        this.$().dataTable({
+
+            "bProcessing": true,
+            "aaData": data,
+
+            "aoColumns": [
+                { "sTitle" : "Name", "mData": "title" },
+                { "sTitle" : "Type", "mData": "type" },
+                { "sTitle" : "Length", "mData": "length" }
+            ]
+        });
+    }
 });
 
-App.ApplicationSerializer = DS.RESTSerializer.extend({
-  primaryKey: '_id',
-});
 
-App.Ride.FIXTURES = [
-  {
-   id: 1,
-   title: 'Haw Ridge',
-   type: 'Mountain',
-   length: 25
-  },
-  {
-    id: 2,
-   title: 'Foothills Parkway',
-   type: 'Road',
-   length: 50
-  },
-  {
-    id: 3,
-    title: 'South Knox',
-    type: 'Mountain',
-    length: 30
-  },
-  {
-    id: 4,
-    title: 'Tsali',
-    type: 'Mountain',
-    length: 20
-  },
-  {
-    id:5,
-    title: 'West Knoxville',
-    type: 'Road',
-    length: 15
-  }
-  ];*/
+
+// App.Ride.FIXTURES = [
+//     {
+//      id: 1,
+//      title: 'Haw Ridge',
+//      type: 'Mountain',
+//      length: 25
+//   },
+//   {
+//     id: 2,
+//    title: 'Foothills Parkway',
+//    type: 'Road',
+//    length: 50
+//   },
+//   {
+//     id: 3,
+//     title: 'South Knox',
+//     type: 'Mountain',
+//     length: 30
+//   },
+//   {
+//     id: 4,
+//     title: 'Tsali',
+//     type: 'Mountain',
+//     length: 20
+//   },
+//   {
+//     id:5,
+//     title: 'West Knoxville',
+//     type: 'Road',
+//     length: 15
+//   }];
